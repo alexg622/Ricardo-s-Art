96070e79fb9fdbd2f16f26c0211fd6f109058047
alexg622 <alex.gm62288@gmail.com>
2017-05-21 15:32:46 -0700

    Make paintings nested route under artists

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,16 +1,19 @@
 class PaintingsController < ApplicationController
 	def index 
-		@paintings = Painting.all 
+		@paintings = Painting.all
+		@artist = Artist.find(current_user.id) 
 	end 
 
 	def new 
 		@painting = Painting.new 
+		@artist = Artist.find(params[:artist_id])
 	end 
 
 	def create 
 		@painting = Painting.new(paintings_params)
+		@artist = Artist.find(params[:artist_id])
 		if @painting.save
-			redirect_to @painting 
+			redirect_to artist_painting_path(@artist, @painting) 
 		else 
 			@errors = @painting.errors.full_messages
 			render 'new'
@@ -23,14 +26,16 @@ class PaintingsController < ApplicationController
 	end 
 
 	def edit
+		@artist = Artist.find(params[:artist_id])
 		@painting = Painting.find(params[:id])
 	end 
 
 	def update 
 		@painting = Painting.find(params[:id])
+		@artist = Artist.find(params[:artist_id])
 		@painting.assign_attributes(paintings_params)
 		if @painting.save
-			redirect_to @painting 
+			redirect_to artist_painting_path(@artist, @painting) 
 		else 
 			@errors = @painting.errors.full_messages
 			render 'edit'
@@ -40,7 +45,7 @@ class PaintingsController < ApplicationController
 	def destroy
 		@painting = Painting.find(params[:id])
 		@painting.delete
-		redirect_to paintings_path
+		redirect_to root_path
 	end  
 
 	def paintings_params
